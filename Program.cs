833e9e26f8b9119d6647a8cabb516a93f193502e
Daniel Stephanio Nunes <dti584@localiza.com>
2023-02-28 11:18:33 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿/*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+X, se for o caso.
+Entrada: 8 ------ Saida: 1,3,5,7*/
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um numero inteiro: \n");
+        int numero = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numero; i++)
+        {
+            if (i % 2 == 1)
+                Console.WriteLine(i);
+        }
+    }
+}
